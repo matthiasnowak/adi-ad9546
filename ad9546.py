e856de0dc45392fc9b7aa08f4386dd6d44212804
matthiasnowak <55640102+matthiasnowak@users.noreply.github.com>
2024-08-06 21:40:33 +0200

    initial changes for ft4222 support

--- ad9546.py
+++ ad9546.py
@@ -5,6 +5,11 @@
 #################################################################
 from smbus import SMBus
 
+import ft4222
+from ft4222.SPI import Cpha, Cpol
+from ft4222.SPIMaster import Mode, Clock, SlaveSelect
+from ft4222.GPIO import Port, Dir
+
 def sign_extend (value, length):
     """ sign extends given integer number to desired length """
     binary = bin(value)[2:]
@@ -24,21 +29,40 @@ class AD9546 :
         address: [int] i2c slave address
         """
         self.slv_addr = address
-        self.handle = SMBus()
-        self.handle.open(bus)
+        # self.handle = SMBus()
+        # self.handle.open(bus)
+        ## ft4222 implementation
+        self.handle = ft4222.openByDescription('FT4222 A')
+        self.handleIO = ft4222.openByDescription('FT4222 B')
+        self.handleIO.setSuspendOut(False)
+        # self.handleIO.gpio_Init(gpio0 = Dir.OUTPUT) # use for reset of pll
+        # self.handleIO.gpio_Write(Port.P0, False)
+        # sleep(0.1)
+        # self.handleIO.gpio_Write(Port.P0, True)
+        self.handle.spiMaster_Init(Mode.SINGLE, Clock.DIV_128, Cpha.CLK_LEADING, Cpol.IDLE_LOW, SlaveSelect.SS0)
     
     def write_data (self, addr, data):
         """ Writes given data (uint8_t) to given address (uint16_t) """
-        msb = (addr & 0xFF00)>>8
-        lsb = addr & 0xFF
-        self.handle.write_i2c_block_data(self.slv_addr, msb, [lsb, data & 0xFF])
+        # msb = (addr & 0xFF00)>>8
+        # lsb = addr & 0xFF
+        # self.handle.write_i2c_block_data(self.slv_addr, msb, [lsb, data & 0xFF])
+        ## ft4222 implementation
+        spidata = (addr << 8) | data
+        spidata = spidata.to_bytes(2, 'big')
+        ft_status = self.handle.spiMaster_SingleWrite(spidata, True)
+
 
     def read_data (self, addr):
         """ Reads data at given address (uint16_t) returns uint8_t """
-        msb = (addr & 0xFF00)>>8
-        lsb = addr & 0xFF
-        self.handle.write_i2c_block_data(self.slv_addr, msb, [lsb])
-        data = self.handle.read_byte(self.slv_addr)
+        # msb = (addr & 0xFF00)>>8
+        # lsb = addr & 0xFF
+        # self.handle.write_i2c_block_data(self.slv_addr, msb, [lsb])
+        # data = self.handle.read_byte(self.slv_addr)
+        ## ft4222 implementation
+        spidata = (addr | 0x8000) << 8  # add extra byte for reading
+        spidata = spidata.to_bytes(3, 'big')
+        data_raw = self.handle.spiMaster_SingleReadWrite(spidata, True)
+        data = int.from_bytes(data_raw[2:], 'big')
         return data
 
     def io_update (self):
